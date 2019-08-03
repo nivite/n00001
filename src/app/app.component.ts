44a2a3fe1198611cac40679a4cc860bf209ca741
Anand <anandchakru@gmail.com>
2019-08-02 17:32:11 -0700

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClogService } from '@nivite/ngsdk';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'n3-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'n00001';
+export class AppComponent implements OnInit {
+  fireconfig = environment.firebase;
+  invite: any;
+  guest: any;
+  constructor(private clog: ClogService) { }
+  ngOnInit() {
+
+  }
+  loadInviteData(invite: any) {
+    this.invite = invite;
+    this.clog.log('invite: ' + (invite ? invite.hostName : invite));
+  }
+  loadUserData(user: any) {
+    this.clog.log('user: ' + (user ? user.email : user));
+  }
+  loadGuestData(guest: any) {
+    this.guest = guest;
+    this.clog.log('guest: ' + (guest ? guest.email : guest));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgsdkLibModule } from '@nivite/ngsdk';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgsdkLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
